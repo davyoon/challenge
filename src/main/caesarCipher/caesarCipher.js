c3acc36aa2a0e596335ee343982fd75160afa9cc
David Yoon <david_yoon86@hotmail.com>
2020-07-24 13:19:05 -0400

    flatlandSpaceStations

--- src/main/caesarCipher/caesarCipher.js
+++ src/main/caesarCipher/caesarCipher.js
@@ -1,1 +1,17 @@
-https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+// https://www.hackerrank.com/challenges/caesar-cipher-1/problem
+
+const caesarCipher = (s, k) => {
+	const shiftedArr = s.split("").map(item => {
+		let charCode = item.charCodeAt();
+		let shiftedVal = charCode + k % 26;
+
+		if(charCode >= 65 && charCode <= 90) {
+			return shiftedVal <= 90 ? String.fromCharCode(shiftedVal) : String.fromCharCode(shiftedVal - 26); 
+		}
+		if(charCode >= 97 && charCode <= 122) {
+			return shiftedVal <= 122 ? String.fromCharCode(shiftedVal) : String.fromCharCode(shiftedVal - 26); 
+		}
+		return item;
+	});
+	return shiftedArr.join("");
+}
